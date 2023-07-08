5efbe7d7c323cfcff7ce38961e6fa06f040b9548
OziOcb <pozyzniewski@yahoo.com>
2023-07-08 10:13:14 +0100

    Add my note inside Auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,8 @@
 export default defineNuxtRouteMiddleware(async (to) => {
+  // NOTE: I had to use the getCurrentUser() method here because using
+  //       const { $auth } = useNuxtApp();
+  //       was breaking the page on browser reload
+  //       This solution was founded here (7:27) - https://www.youtube.com/watch?v=xceR7mrrXsA
   const { getCurrentUser } = useFirebaseAuth();
   const currentUser = await getCurrentUser();
   if (!currentUser?.uid && to.path !== "/") {
