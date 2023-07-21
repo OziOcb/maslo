71e129f348fde4578d21ec77953d75c3393d5ea0
OziOcb <pozyzniewski@yahoo.com>
2023-07-21 20:48:28 +0100

    Recreate the login form

--- components/FormLogin.vue
+++ components/FormLogin.vue
@@ -1,37 +1,72 @@
 <template>
-  <VCard title="Login" class="elevation-5">
-    <VCardText>
-      <!-- TODO: ENDED HERE! Use Vuetify's inputs below and then recreate the SignUp form (make sure that the submit buttons are disabled if inputs are empty) -->
-      <input type="email" placeholder="Email" required v-model="email" />
-      <input
-        type="password"
-        placeholder="Password"
-        required
-        v-model="password"
-      />
-    </VCardText>
+  <VCard
+    class="mx-auto pa-12 pb-8"
+    width="100%"
+    elevation="8"
+    max-width="448"
+    rounded="lg"
+  >
+    <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-    <VCardActions>
-      <VBtn
-        class="flex-grow-1"
-        color="primary"
-        variant="outlined"
-        text="Login"
-        :loading="isLoading"
-        @click="loginHandler"
-      />
-    </VCardActions>
-  </VCard>
+    <VTextField
+      v-model="email"
+      :disabled="isLoading"
+      density="compact"
+      placeholder="Email address"
+      prepend-inner-icon="mdi-email-outline"
+      variant="outlined"
+    />
 
-  <div class="mt-4">
-    No account yet?
+    <!-- TODO: Add Forgot Password functionality -->
+    <!-- <div
+      class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+    >
+      Password
+
+      <a
+        class="text-caption text-decoration-none text-blue"
+        href="#"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Forgot login password?</a
+      >
+    </div> -->
+
+    <VTextField
+      v-model="password"
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="isPasswordVisible ? 'text' : 'password'"
+      :disabled="isLoading"
+      density="compact"
+      placeholder="Enter your password"
+      prepend-inner-icon="mdi-lock-outline"
+      variant="outlined"
+      @click:append-inner="isPasswordVisible = !isPasswordVisible"
+    />
 
     <VBtn
+      :loading="isLoading"
+      :disabled="isDisabled"
+      block
+      class="mb-8"
+      color="blue"
+      size="large"
       variant="tonal"
-      text="Sign Up"
-      @click="$emit('toggleShowLogin', false)"
+      text="Log In"
+      @click="loginHandler"
     />
-  </div>
+
+    <VCardText class="text-center">
+      <a
+        class="text-blue text-decoration-none"
+        href="#"
+        @click="!isLoading ? $emit('toggleShowLogin', false) : null"
+      >
+        Sign up now <VIcon icon="mdi-chevron-right" />
+      </a>
+    </VCardText>
+  </VCard>
 </template>
 
 <script setup>
@@ -41,7 +76,12 @@ const { signInUser } = useFirebaseAuth();
 
 const email = ref("oz@oz.com");
 const password = ref("deskorolka");
+const isPasswordVisible = ref(false);
 const isLoading = ref(false);
+
+const isDisabled = computed(() => {
+  return !email.value || !password.value;
+});
 
 async function loginHandler() {
   isLoading.value = true;
