964b06a61e102f52c30001ad6bb186f1516d7c97
OziOcb <pozyzniewski@yahoo.com>
2023-07-21 23:52:37 +0100

    backup

---
+++ pages/dashboard/players.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- TODO: ENDED HERE! Make links below dynamic / Finish styling this page -->
+  <VTabs direction="vertical" color="primary">
+    <VTab to="/dashboard/players/list-123" value="1"> Option 1 </VTab>
+    <VTab to="/dashboard/players/list-456" value="2"> Option 2 </VTab>
+  </VTabs>
+
+  <NuxtPage />
+</template>
+
+<script setup lang="ts"></script>
--- pages/dashboard/players/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h3>players</h3>
-</template>
-
-<script setup lang="ts"></script>
